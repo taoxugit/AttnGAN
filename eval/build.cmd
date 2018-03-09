1e56244e3f2f820e8129d6336129bcd52758a948
sethjuarez <me@sethjuarez.com>
2018-03-08 17:19:43 -0800

    change curl command to point to azure

--- eval/build.cmd
+++ eval/build.cmd
@@ -1,3 +1,3 @@
 docker build -t "attngan" .
 REM docker run -it -e BLOB_KEY=KEY -p 5678:8080 attngan
-REM curl -H "Content-Type: application/json" -X POST -d '{"caption":"the bird has a yellow crown and a black eyering that is round"}' http://localhost:5678/attgan/api/v1.0/bird
+REM curl -H "Content-Type: application/json" -X POST -d '{"caption":"the bird has a yellow crown and a black eyering that is round"}' https://attgan.azurewebsites.net/api/v1.0/bird
